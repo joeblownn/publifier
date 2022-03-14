8c265938fed2e92951046cc63591f78c127bb09f
JohnnySD <ba004629@bac.qld.edu.au>
2022-03-14 10:57:41 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,9 @@
 import os, re
-re.compile('[\W_]+', re.UNICODE)
-cls = lambda: os.system('cls')
-print('')
 while True:
-	full = ''
-	line = ' '
-	while line != '':
+	print('')
+	full = line = '\n'
+	while not full.endswith('\n\n'):
 		line = input('»')
-		if line != '':
-			if not full:
-				full = line
-			else:
-				full += f'\n{line}'
-	full = re.sub('[\W_]+', ' ', full.lower(), flags=re.UNICODE)
-	cls()
-	print(f'\n{full}\n')
+		full += f'\n{line}'
+	os.system('cls')
+	print(re.sub('[\W_]+', ' ', full.lower(), flags=re.UNICODE))
